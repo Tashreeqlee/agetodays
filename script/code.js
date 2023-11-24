e3d415d3f8ebafd79e696b3e6f6a364b5497b6bd
Tashreeqlee <tashyct14@gmail.com>
2023-11-24 09:49:00 +0200

    js added

--- script/code.js
+++ script/code.js
@@ -1,1 +1,11 @@
-//
+document.getElementById("calculateButton").addEventListener("click", function() {
+    var ageInput = document.getElementById("ageInput").value;
+    var result = document.getElementById("result");
+    
+    if (ageInput !== "") {
+        var ageInDays = ageInput * 365;
+        result.innerHTML = "Your age in days is: " + ageInDays;
+    }else {
+        result.innerHTML = "Please enter your age.";
+    }
+});
